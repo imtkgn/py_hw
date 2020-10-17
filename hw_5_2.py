a0b9ae89d7f27ba6f92e556a71d5685e68d4491c
IgorTarasov <imtkgn@gmail.com>
2020-10-18 01:51:13 +0300

    edited the code

--- hw_5_2.py
+++ hw_5_2.py
@@ -3,9 +3,10 @@
     выполнить подсчет количества строк, количества слов в каждой строке.
 """
 with open("hw_5_2_file.txt") as f_obj:
-    lines_count, words_count = 0, 0
+    lines_count, words_count_all, words_cnt_line = 0, 0, 0
     for line in f_obj:
         lines_count += 1
-        words_count += line.count(" ")
-        print(f"Строка {lines_count}: {line.count(' ')} слов")
-    print(f"ИТОГО: строк - {lines_count}, слов в строках - {words_count}")
+        words_cnt_line = len(line.split(" ")) - line.count('\n')
+        words_count_all += words_cnt_line
+        print(f"Строка {lines_count}: {words_cnt_line} слов -> {line.split(' ')}")
+    print(f"ИТОГО: строк - {lines_count}, слов в строках - {words_count_all}")
